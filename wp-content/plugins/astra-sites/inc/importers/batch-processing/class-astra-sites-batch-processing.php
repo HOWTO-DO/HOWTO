b73bf4fc3d1944b2581334d5a063b762ed98059b
L <suzleticia@gmail.com>
2023-01-02 21:37:11 +0300

    first www

---
+++ wp-content/plugins/astra-sites/inc/importers/batch-processing/class-astra-sites-batch-processing.php
@@ -0,0 +1,126 @@
+<br>
+<font size="1"><table class="xdebug-error xe-warning" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Warning: Use of undefined constant ABSPATH - assumed 'ABSPATH' (this will throw an Error in a future version of PHP) in C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php on line <i>122</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>0</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">2</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing::get_instance(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>1007</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">3</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->__construct(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>71</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">4</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->includes(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>83</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-warning" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Warning: require_once(ABSPATHwp-admin/includes/image.php): failed to open stream: No such file or directory in C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php on line <i>122</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>0</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">2</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing::get_instance(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>1007</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">3</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->__construct(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>71</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">4</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->includes(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>83</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-fatal-error" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: require_once(): Failed opening required 'ABSPATHwp-admin/includes/image.php' (include_path='.;C:\php\pear') in C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php on line <i>122</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>0</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">2</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361296</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing::get_instance(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>1007</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">3</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->__construct(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>71</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">4</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">361352</td>
+<td bgcolor="#eeeeec">Astra_Sites_Batch_Processing->includes(  )</td>
+<td title="C:\wamp64\www\HOWTO\wp-content\plugins\astra-sites\inc\importers\batch-processing\class-astra-sites-batch-processing.php" bgcolor="#eeeeec">...\class-astra-sites-batch-processing.php<b>:</b>83</td>
+</tr>
+</table></font>
